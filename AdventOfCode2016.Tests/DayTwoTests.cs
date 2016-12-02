4c8e7871fda71cee62f54a3e8a84826b9af3aa12
Tom Wallace <tpw@AIMR.ORG>
2016-12-02 14:02:01 -0500

    Day 2 puzzle solutions.

---
+++ AdventOfCode2016.Tests/DayTwoTests.cs
@@ -0,0 +1,49 @@
+﻿using Xunit;
+
+namespace AdventOfCode2016.Tests
+{
+    public class DayTwoTests
+    {
+        [Theory]
+        [InlineData("2", "U")]
+        [InlineData("1", "ULLL")]
+        public void testDetermineBathroomCodeSimple(string expected, string input)
+        {
+            var sot = new DayTwo();
+            var result = sot.DetermineBathroomCode(input, 1, 1, DayTwo.KEY_PAD);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void testDetermineBathroomCode()
+        {
+            string input = @"ULL
+RRDDD
+LURDL
+UUUUD";
+            var sot = new DayTwo();
+            var result = sot.DetermineBathroomCode(input, 1, 1, DayTwo.KEY_PAD);
+
+            Assert.Equal("1985", result);
+        }
+
+        [Fact]
+        public void testWithActualPartA()
+        {
+            var sot = new DayTwo();
+            var result = sot.DetermineBathroomCode(DayTwo.PUZZLE_INPUT, 1, 1, DayTwo.KEY_PAD);
+
+            Assert.Equal("24862", result);
+        }
+
+        [Fact]
+        public void testWithActualPartB()
+        {
+            var sot = new DayTwo();
+            var result = sot.DetermineBathroomCode(DayTwo.PUZZLE_INPUT, 1, 1, DayTwo.KEY_PAD_TWO);
+
+            Assert.Equal("46C91", result);
+        }
+    }
+}
---
+++ AdventOfCode2016/DayTwo.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2016
+{
+    public class DayTwo
+    {
+        public static string PUZZLE_INPUT = @"LUULRUULULLUDUDULDLUDDDLRURUDLRRDRDULRDDULLLRULLLURDDLRDLUUDDRURDDRDDDDRDULULLLLURDDLLRLUUDDDRLRRRDURLDDLRRLDUDRRRDLDLRRDLDLUURRLRULLULRUDRDLRUURLDRDLRLDULLLUDRDDRLURLUUDRLLLDRUUULLUULRUDDUDRDUURRRUDRLDDUURDUURUDRDDLULDDUDUDRRDDULUDULRDRULRLRLURURDULRUULLRDDDDRRUUDDDUUDRLLRUDRLRDLRRLULRLULRUDDULRLLLURLDDRLDDLRRLDRDDDRRLRUDRULUUDUURLDLRRULUDRDULDLLRRURRDDLRRRLULUDUUDDUDDLRDLRDRLRLDUDUDDUDLURRUURDRLRURLURRRLRLRRUDDUDDLUDRLUURUUDUUDDULRRLUUUDRLRLLUR
+LDLLRRLDULDDRDDLULRRRDDUDUDRRLLRUUULRUDLLRRDDRRLDDURUUDLUDRRLDURDDRUDLUDUUDLDLLLDLLLDRLLDLRUULULLUUDULDUUULDDLRUDLLUDLUUULDRLUDRULUUDLDURDLDUULLRDUDRDLURULDLUUUDURLDDRLLDRLRDDDUDRUULLDLUDRRDDLDLUURUDDLDRURRLULUDDURLDRDRDUDDRRULRLDURULULRURDUURRUDRDDRDRLDRDUUDLRULRDDDULRURUDRUUULUUDDLRRDDDUDRLRUDRDLRRUDLUDRULDDUDLRLDDLDRLRDLULRDRULRLLRLUDUURULLLDDUULUUDDDUDRRULDDDULRUDRRLRLLLUDLULDUUULDDULDUUDLUULRDLDUDRUDLLDLDLLULDDDDLUDDUDRUDLRRRDDDDDLLRRDRUUDDDRRULRUDUUDRULLDLLLDDRDDUURLUUURUDRUDURLRUUUULUUURDRRRULDUULDLDDDRDDDDLLDRUDRDURLDDURDURULDDRLLRRLDUDRDURRLDRDLLULUUUD
+LDDLRLRDDRLRUDDRDDUDRULUUULULDULRUULLRRDUULRDUUDDDRRULDDUDRLLLDULURDLDDRLLRURULULDLDULRDLDLRULUDLLDRUDLDURRDULDDRLRURDLLUDRDDDUDLUDULURULRDRLRULDLLRLDRRUDRDRUDRLDLRLUUURURRRLDDULLULLLRLRLULDLLRLDDRLDULURULRUURRUUURRUDRLRRURURDDDRULDULDLDLRRRLLDDRRURRULULULDRDULDRRULDUDRRLDULDRDURRDULLRRRLLLLRRLLRRRDRURDUULLURURURDDRRDRLLLULRRRDRLDRLDRDLLRUUDURRDRRDLLUDLDRLRLDLUDRDULRULRRLLRDLULDRLUDUUULLDRULDDLLRDUUUDRUUUUULUURDDLLDUURURRURLLURRDDUDUDRUUDDRDDRRLRLULRLRRRDRLLRRLLLDUULLUUDDLULLLDURRLLDRLDRDRLRRLRRULRRRRLRRRRRURUDULUULRDLLDRLRRDUURDRRUDRURRRDDRLDDLRLUDRDRDRRLDDDRDDRRRDUDULRURRDRDLLDRUD
+UUUDLDDLRDLLLLRUUURDDLLURRUUURLUULLURUUDUDLDULULLRRRRLLLRDLLUDRUURDRURUDRURRLRLDRURLUDRLULRRURDDDURLLDULDLRRRDUUDDDRDLRUURRDRDRLRDLULRLDDRULRULDRDUDRUURLDLUDDULLLRURRLURLULDRRLUUURURLDLDDULLLRUUURDDDUURULULLUUUDUDRLLRRULUULDDDLLUDLURLLLRRULLURDRLUUDDLLDLLLUDULLRDRRRURDRUDUDUULUDURDLRUDLLRDDRURUDURLRULURDDURULLRDDRLRRDRLLULRDDDULRDLRULDDLRRDULDLUURRURUULRRDUURUDRRRRRLDULDLRURRULULDLRDDDRLLDURRULDUDUDRRRLUULRLUDURRRLRLDURRRRUULDRLUDDDUDURLURUDLLUDRDDDRLLURLRLDDURUUDDDUDUR
+RURRRRURUDDRLURUDULRDUDDDUURULDRRRRURDLDRRLLDLUDLRRLRRUULLURULLRDLLRDDDDULLRLLDDLLRUDDULDUDLDURLRUULDDURURDURDLDRRULRURRRRRLRRLLUDURRURULRLRDLRLRRRLLURURDLLLDLDDULDLUDDLLLRUDDRDRLRUDRRLDDLRDLRLRLRLRRDUUURRUDRRLDLRRUULULLUDRRRUDLURDRUULDRDRRLUULULDDLURRLDULLURLDRLDULDRLLDLUUULLULRRDDRURRURLDLDRRLLLLLUDUURUULURLRDDDLRRRRLLLURUDLDDRDDRRUDURUULDRRULLLRRLRULLLRLDDLLRRLRURLRDRUDULLDDLDDDDDLDURURDLULRDDLRDLLRURLLRDLRUDDRDRRDURDURLUDRLDUDDDRRURRLUULURULLRLRDLRRLRURULLDDURLLRRRUDDRDLULURRRUUUULUULRRLLDLRUUURLLURLUURRLRL";
+
+        public static string[,] KEY_PAD = new string[,] { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
+
+        public static string[,] KEY_PAD_TWO = new string[,]
+        {
+            { "", "", "1", "", "" },
+            { "", "2", "3", "4", "" },
+            { "5", "6", "7", "8", "9" },
+            { "", "A", "B", "C", "" },
+            { "", "", "D", "", "" }
+        };
+
+        public string DetermineBathroomCode(string input, int horizontal, int vertical, string[,] keyPad)
+        {
+            string result = "";
+
+            List<string> lineInputs = input.Split(new string[] { "\n", "\r\n" }, StringSplitOptions.RemoveEmptyEntries).ToList<string>();
+            foreach (string lineInput in lineInputs)
+            {
+                List<char> steps = lineInput.ToList<char>();
+                foreach (char step in steps)
+                {
+                    if (step == 'U')
+                    {
+                        vertical = HandleOutOfBoundsVertical(vertical, horizontal, -1, keyPad);
+                    }
+                    else if (step == 'L')
+                    {
+                        horizontal = HandleOutOfBoundsHorizontal(vertical, horizontal, -1, keyPad);
+                    }
+                    else if (step == 'R')
+                    {
+                        horizontal = HandleOutOfBoundsHorizontal(vertical, horizontal, 1, keyPad);
+                    }
+                    else if (step == 'D')
+                    {
+                        vertical = HandleOutOfBoundsVertical(vertical, horizontal, 1, keyPad);
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Not recongized direction");
+                    }
+                }
+
+                result += keyPad[vertical, horizontal];
+            }
+
+            return result;
+        }
+
+        // Handle out of bounds exceptions - VERTICAL
+        private int HandleOutOfBoundsVertical(int vertical, int horizontal, int change, string[,] keyPad)
+        {
+            int newValue = vertical + change;
+            if (newValue < 0 || newValue >= keyPad.GetLength(0))
+                return vertical;
+
+            // Check there is a value there
+            string keyPadEntry = keyPad[newValue, horizontal];
+            if (keyPadEntry == "")
+                return vertical;
+
+            return newValue;
+        }
+
+        // Handle out of bounds exceptions - HORIZONTAL
+        private int HandleOutOfBoundsHorizontal(int vertical, int horizontal, int change, string[,] keyPad)
+        {
+            int newValue = horizontal + change;
+            if (newValue < 0 || newValue >= keyPad.GetLength(0))
+                return horizontal;
+
+            // Check there is a value there
+            string keyPadEntry = keyPad[vertical, newValue];
+            if (keyPadEntry == "")
+                return horizontal;
+
+            return newValue;
+        }
+    }
+}

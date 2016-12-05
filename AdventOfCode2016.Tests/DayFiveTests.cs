16da113b9355120b842b5e3aaddbffda87fc0a9d
Tom Wallace <thomaspwallace@gmail.com>
2016-12-05 12:27:32 -0500

    Day 5 solutions completed.

---
+++ AdventOfCode2016.Tests/DayFiveTests.cs
@@ -0,0 +1,77 @@
+﻿using System.Security.Cryptography;
+using Xunit;
+
+namespace AdventOfCode2016.Tests
+{
+    public class DayFiveTests
+    {
+        // Note that this test takes a while to run, so be patient or comment out
+        /*
+        [Fact]
+        public void testDetermineSecurityDoorPassword()
+        {
+            string input = "abc";
+            var sot = new DayFive();
+            var result = sot.DetermineSecurityDoorPassword(input);
+
+            Assert.Equal("18f47a30", result);
+        }
+        */
+
+        // Note that this test takes a while to run, so be patient or comment out
+        /*
+        [Fact]
+        public void testDetermineSecurityDoorPasswordPartTwo()
+        {
+            string input = "abc";
+            var sot = new DayFive();
+            var result = sot.DetermineSecurityDoorPasswordPartTwo(input);
+
+            Assert.Equal("05ace8e3", result);
+        }
+        */
+
+        [Fact]
+        public void testGetMd5Hash()
+        {
+            string input = "abc3231929";
+            MD5 md5Hash = MD5.Create();
+            var sot = new DayFive();
+            var result = sot.GetMd5Hash(md5Hash, input);
+
+            Assert.True(result.Substring(0, 5) == "00000");
+        }
+
+        [Theory]
+        [InlineData("--------", "000001bcde", "-b------")]
+        [InlineData("-b------", "000001zcde", "-b------")]
+        [InlineData("-b------", "000000bcde", "bb------")]
+        [InlineData("-b------", "00000fbcde", "-b------")]
+        [InlineData("-b------", "000009bcde", "-b------")]
+        public void testHandleResultUpdate(string currentPassword, string currentHash, string expected)
+        {
+            var sot = new DayFive();
+            var result = sot.HandleResultUpdate(currentPassword, currentHash);
+
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void testWithActualPartA()
+        {
+            var sot = new DayFive();
+            var result = sot.DetermineSecurityDoorPassword(DayFive.PUZZLE_INPUT);
+
+            Assert.Equal("d4cd2ee1", result);
+        }
+
+        [Fact]
+        public void testWithActualPartB()
+        {
+            var sot = new DayFive();
+            var result = sot.DetermineSecurityDoorPasswordPartTwo(DayFive.PUZZLE_INPUT);
+
+            Assert.Equal("f2c730e5", result);
+        }
+    }
+}
---
+++ AdventOfCode2016/DayFive.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace AdventOfCode2016
+{
+    public class DayFive
+    {
+        public static string PUZZLE_INPUT = "ugkcyxxp";
+
+        // Determine the security door password by iterating through MD5 hashs looking for hashes that begin with "00000"
+        // Then use the next character to collect the password
+        public string DetermineSecurityDoorPassword(string input)
+        {
+            string result = "";
+
+            long counter = -1;
+
+            MD5 md5Hash = MD5.Create();
+
+            // Loop until we have our full password of 8 characters
+            do
+            {
+                ++counter;
+
+                string hashInput = input + counter;
+                string resultHash = GetMd5Hash(md5Hash, hashInput);
+
+                // Check to see if the resultHash is valid - i.e. starts with 5 zeros
+                if (resultHash.Substring(0, 5) == "00000")
+                {
+                    // Then add the sixth character to accumulating password
+                    result += resultHash.Substring(5, 1);
+                }
+            } while (result.Length < 8);
+
+            return result;
+        }
+
+        // Updates the password by iterating the MD5 hashes, like the previous problem.  But, now the 6th character tells you the
+        // digit of the password to update with the 7th character.
+        public string DetermineSecurityDoorPasswordPartTwo(string input)
+        {
+            string result = "--------";
+
+            long counter = -1;
+
+            MD5 md5Hash = MD5.Create();
+
+            // Loop until we have our full password, i.e. one that no longer has -'s in it
+            do
+            {
+                ++counter;
+
+                string hashInput = input + counter;
+                string resultHash = GetMd5Hash(md5Hash, hashInput);
+
+                // Check to see if the resultHash is valid - i.e. starts with 5 zeros
+                if (resultHash.Substring(0, 5) == "00000")
+                {
+                    // Then add the sixth character to accumulating password
+                    result = HandleResultUpdate(result, resultHash);
+                }
+            } while (result.IndexOf('-') != -1);
+
+            return result;
+        }
+
+        // Hash function from MSDN - https://msdn.microsoft.com/en-us/library/system.security.cryptography.md5%28v=vs.110%29.aspx
+        public string GetMd5Hash(MD5 md5Hash, string hashInput)
+        {
+            // Convert the input string to a byte array and compute the hash.
+            byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(hashInput));
+
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data
+            // and format each one as a hexadecimal string.
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            // Return the hexadecimal string.
+            return sBuilder.ToString();
+        }
+
+        // Handle updates to the currentPassword, if appropriate
+        public string HandleResultUpdate(string currentPassword, string currentHash)
+        {
+            StringBuilder sb = new StringBuilder(currentPassword);
+            string stringDigit = currentHash.Substring(5, 1);
+            int digitToUpdate;
+
+            if (Int32.TryParse(stringDigit, out digitToUpdate) == false)
+                return sb.ToString();
+
+            if (digitToUpdate < 0 || digitToUpdate > 7)
+                return sb.ToString();
+
+            // If digit has not already been updated, update it
+            if (currentPassword.Substring(digitToUpdate, 1) == "-")
+            {
+                // If so, then update with the seventh character in string
+                sb[digitToUpdate] = currentHash.Substring(6, 1)[0];
+            }
+
+            return sb.ToString();
+        }
+    }
+}

832899143b6191be8f01e939f156ecccc86c5bca
Tom Wallace <thomaspwallace@gmail.com>
2016-12-09 00:24:42 -0500

    Adding Day 8 solutions and tests.

---
+++ AdventOfCode2016.Tests/DayEightTests.cs
@@ -0,0 +1,95 @@
+﻿using AdventOfCode2016.Eight;
+using Xunit;
+
+namespace AdventOfCode2016.Tests
+{
+    public class DayEightTests
+    {
+        [Fact]
+        public void testRect()
+        {
+            char[,] screen = { { '.', '.', '.', '.', '.', '.', '.' },
+                               { '.', '.', '.', '.', '.', '.', '.' },
+                               { '.', '.', '.', '.', '.', '.', '.' }};
+
+            var input = "rect 3x2";
+            var sot = new DayEight();
+            sot.Rect(input, screen);
+
+            var litPixels = sot.CountLitPixelsOnScreen(screen);
+
+            Assert.Equal(6, litPixels);
+        }
+
+        [Fact]
+        public void testRect_ExistingLit()
+        {
+            char[,] screen = { { '.', '.', '.', '.', '.', '*', '.' },
+                               { '.', '.', '.', '.', '.', '*', '.' },
+                               { '.', '.', '.', '.', '.', '*', '.' }};
+
+            var input = "rect 3x2";
+            var sot = new DayEight();
+            sot.Rect(input, screen);
+
+            var litPixels = sot.CountLitPixelsOnScreen(screen);
+
+            Assert.Equal(9, litPixels);
+        }
+
+        [Fact]
+        public void testRotateRow()
+        {
+            char[,] screen = { { '.', '.', '.', '.', '.', '.', '.' },
+                               { '.', '.', '*', '.', '*', '*', '*' },
+                               { '.', '.', '.', '.', '.', '.', '.' }};
+
+            var input = "rotate row y=1 by 2";
+            var sot = new DayEight();
+            sot.RotateRow(input, screen);
+
+            Assert.Equal('*', screen[1, 0]);
+            Assert.Equal('*', screen[1, 1]);
+            Assert.Equal('.', screen[1, 2]);
+            Assert.Equal('.', screen[1, 3]);
+            Assert.Equal('*', screen[1, 4]);
+            Assert.Equal('.', screen[1, 5]);
+            Assert.Equal('*', screen[1, 6]);
+
+            var litPixels = sot.CountLitPixelsOnScreen(screen);
+            Assert.Equal(4, litPixels);
+        }
+
+        [Fact]
+        public void testRotateColumn()
+        {
+            char[,] screen = { { '.', '.', '.', '.', '.', '.', '.' },
+                               { '.', '.', '*', '.', '.', '.', '.' },
+                               { '.', '.', '*', '.', '.', '.', '.' }};
+
+            var input = "rotate column x=2 by 1";
+            var sot = new DayEight();
+            sot.RotateColumn(input, screen);
+
+            Assert.Equal('*', screen[0, 2]);
+            Assert.Equal('.', screen[1, 2]);
+            Assert.Equal('*', screen[2, 2]);
+
+            var litPixels = sot.CountLitPixelsOnScreen(screen);
+            Assert.Equal(2, litPixels);
+        }
+
+        [Fact]
+        public void testWithActualPartA()
+        {
+            var sot = new DayEight();
+            var result = sot.CountNumberOfLitPixels();
+
+            Assert.Equal(116, result);
+
+            sot.PrintScreen(sot.partAScreen);
+            // Result of the print screen was = UPOJFLBCEZ
+            // Had to copy into a text editor to zoom and see better
+        }
+    }
+}
---
+++ AdventOfCode2016/Eight/DayEight.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace AdventOfCode2016.Eight
+{
+    public class DayEight
+    {
+        public readonly char LIT = '*';
+
+        // 50 pixels wide and 5 tall
+        public char[,] partAScreen = { { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', },
+                                       { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', },
+                                       { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', },
+                                       { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', },
+                                       { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', },
+                                       { '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', '.', }};
+
+        public long CountNumberOfLitPixels()
+        {
+            string line;
+            StreamReader file = new StreamReader(@"Eight\DayEightInput.txt");
+
+            // Iterate over each line in the input
+            while ((line = file.ReadLine()) != null)
+            {
+                if (line.Contains("rect"))
+                    Rect(line, partAScreen);
+
+                if (line.Contains("row"))
+                    RotateRow(line, partAScreen);
+
+                if (line.Contains("column"))
+                    RotateColumn(line, partAScreen);
+            }
+            file.Close();
+
+            return CountLitPixelsOnScreen(partAScreen);
+        }
+
+        public void Rect(string input, char[,] screen)
+        {
+            // Parse key values - rect AxB
+            List<string> sections = input.Split(' ').ToList<string>();
+            List<string> numbers = sections[1].Split('x').ToList<string>();
+            int width = Int32.Parse(numbers[0]);
+            int height = Int32.Parse(numbers[1]);
+
+            // Update the screen
+            for (int h = 0; h < height; h++)
+            {
+                for (int w = 0; w < width; w++)
+                {
+                    screen[h, w] = LIT;
+                }
+            }
+        }
+
+        public void RotateRow(string input, char[,] screen)
+        {
+            // Parse key values - rect AxB
+            List<string> sections = input.Split('=').ToList<string>();
+            List<string> numbers = sections[1].Split(new string[] { " by " }, StringSplitOptions.RemoveEmptyEntries).ToList<string>();
+            int row = Int32.Parse(numbers[0]);
+            int right = Int32.Parse(numbers[1]);
+
+            // Update the screen
+            for (int shift = 0; shift < right; shift++)
+            {
+                // Right most value to not double save it
+                char rightMostValue = screen[row, screen.GetLength(1) - 1];
+                for (int i = screen.GetLength(1) - 1; i >= 0; i--)
+                {
+                    if (i == 0)
+                    {
+                        screen[row, i] = rightMostValue;
+                    }
+                    else
+                    {
+                        char leftPixelChar = screen[row, i - 1];
+                        screen[row, i] = leftPixelChar;
+                    }
+                }
+            }
+        }
+
+        public void RotateColumn(string input, char[,] screen)
+        {
+            // Parse key values - rect AxB
+            List<string> sections = input.Split('=').ToList<string>();
+            List<string> numbers = sections[1].Split(new string[] { " by " }, StringSplitOptions.RemoveEmptyEntries).ToList<string>();
+            int column = Int32.Parse(numbers[0]);
+            int down = Int32.Parse(numbers[1]);
+
+            // Update the screen
+            for (int shift = 0; shift < down; shift++)
+            {
+                // Bottom most value to not double save it
+                char bottomMostValue = screen[screen.GetLength(0) - 1, column];
+                for (int i = screen.GetLength(0) - 1; i >= 0; i--)
+                {
+                    if (i == 0)
+                    {
+                        screen[i, column] = bottomMostValue;
+                    }
+                    else
+                    {
+                        char upperPixelChar = screen[i - 1, column];
+                        screen[i, column] = upperPixelChar;
+                    }
+                }
+            }
+        }
+
+        public long CountLitPixelsOnScreen(char[,] screen)
+        {
+            long count = 0;
+
+            // Iterate over the pixels
+            for (int h = 0; h < screen.GetLength(0); h++)
+            {
+                for (int w = 0; w < screen.GetLength(1); w++)
+                {
+                    if (screen[h, w] == LIT)
+                        count++;
+                }
+            }
+
+            return count;
+        }
+
+        public void PrintScreen(char[,] screen)
+        {
+            for (int h = 0; h < screen.GetLength(0); h++)
+            {
+                string line = "";
+                for (int w = 0; w < screen.GetLength(1); w++)
+                {
+                    line += screen[h, w];
+                }
+                Debug.WriteLine(line);
+                string placeholder = "Gives a place for a breakpoint to stop and look at the output";
+            }
+        }
+    }
+}
